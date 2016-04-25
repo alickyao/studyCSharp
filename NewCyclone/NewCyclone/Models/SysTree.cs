4bcc02b498fe4bbbd3bfcee0ee02811da6c23ed4
alickyao <7181210@qq.com>
2016-04-25 22:10:34 +0800

    new

---
+++ NewCyclone/NewCyclone/Models/SysTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewCyclone.Models
+{
+    /// <summary>
+    /// 树
+    /// </summary>
+    public class SysTree
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 父节点
+        /// </summary>
+        public string _parentId { get; set; }
+    }
+
+    /// <summary>
+    /// 系统菜单项
+    /// </summary>
+    public class SysMenu : SysTree {
+
+
+        /// <summary>
+        /// 可访问的角色
+        /// </summary>
+        public string[] roles { get; set; }
+
+        /// <summary>
+        /// 对应的链接
+        /// </summary>
+        public string url { get; set; }
+
+        /// <summary>
+        /// 子节点
+        /// </summary>
+        public LinkedList<SysMenu> child { get; set; }
+    }
+}
